9e1228d1c67327684d38c47536da3d7d00edab5b
Wheaxy Sueido <joshgunvk@code.edu.az>
2024-05-15 22:24:40 +0400

    Tapsirig 1 bitti

---
+++ task15052024/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = 10;
+        int number2 = 35 ;
+        int counter = 0;
+        for (int i = number1; i <= number2; i++)
+        {
+            if (i % 2 != 0)
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine(counter);
+    }
+}
