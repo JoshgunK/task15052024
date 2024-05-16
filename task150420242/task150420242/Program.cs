4733429ad832dd49b7fe19d2bb37fa2bc3b52f15
Joshgun <169159204+JoshgunK@users.noreply.github.com>
2024-05-16 23:48:20 +0400

    Task 2 Successfully Completed

---
+++ task150420242/task150420242/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Code Academy";
+        int[] charcounter = new int[256];
+        foreach (char c in text)
+        {
+            charcounter [(int)c]++; 
+        }
+        for (int i = 0; i < charcounter.Length; i++)
+        {
+            if (charcounter[i] > 0)
+            {
+                Console.WriteLine($"{(char)i} - {charcounter[i]}");
+            }
+        }
+    }
+}
